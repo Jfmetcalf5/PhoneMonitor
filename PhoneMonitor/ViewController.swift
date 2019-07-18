0ec1165937410d7564a6618364b64e26fdafd9ae
Jacob <Jfmetcalf5@gmail.com>
2019-07-18 08:06:32 -0600

    Initial Commit

---
+++ PhoneMonitor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhoneMonitor
+//
+//  Created by Jacob Metcalf on 7/18/19.
+//  Copyright © 2019 Jacob Metcalf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
